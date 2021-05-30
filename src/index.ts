6ba2b3d56e0806e73a8520b20b92800ee0e4cd45
Charles Anderson <hpcngmoh@gmail.com>
2021-05-30 18:55:43 -0400

    preparing for npm

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export const promise_then_catch = <T>(e: Promise<T> | (() => Promise<T>)) =>
-    (typeof e === "function" ? e() : e).catch(err => console.log(err)) && null;
+const promise_then_catch = <T>(e: Promise<T> | (() => Promise<T>)) =>
+  (typeof e === 'function' ? e() : e).catch((err) => console.log(err)) && null;
+
+export default promise_then_catch;
